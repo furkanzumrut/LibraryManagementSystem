c12ad6e1d61ebd0144fe7700e00298fb40b05a74
Furkan Zumrut <furkan@furkanzumrut.com>
2015-06-02 18:28:16 +0300

    MongoDB Configuration Updated

--- src/main/java/com/librarymanagementsystem/config/MongoConfiguration.java
+++ src/main/java/com/librarymanagementsystem/config/MongoConfiguration.java
@@ -1,11 +1,15 @@
 package com.librarymanagementsystem.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 import com.mongodb.Mongo;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 
 @Configuration
 @EnableMongoRepositories
@@ -13,12 +17,14 @@ public class MongoConfiguration extends AbstractMongoConfiguration {
 
     @Override
     protected String getDatabaseName() {
-        return "librarymanagementDB";
+        return "heroku_app37356552";
     }
 
     @Override
     public Mongo mongo() throws Exception {
-        return new MongoClient("127.0.0.1", 27017);
+    	MongoCredential credential = MongoCredential.createMongoCRCredential("heroku_rebios", getDatabaseName(), "199300".toCharArray());
+
+        return new MongoClient(new ServerAddress("ds031631.mongolab.com:31631"),Arrays.asList(credential));
     }
 
     @Override
